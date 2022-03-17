84a28750649ca260ddd1de88642d6a2e4e3973f1
Jishnu-Nandhiath <jishnun789@gmail.com>
2022-03-17 21:59:32 +0530

    inital commit

---
+++ Expense_Monitor/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Let's get started!</h1>
+      <p> div in here is to group together multiple elements, he saw that coming</p>
+    </div>
+      );
+}
+
+export default App;
